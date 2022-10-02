7c6de7cdec8249f9a2eb999e06c6fb35aea5cc89
otisnth <otisnth@gmail.com>
2022-10-02 12:07:55 +0300

    added counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,42 @@
 document.querySelectorAll('a[href^="#"').forEach(link =>{
     link.addEventListener('click', function(e) {
-        e.preventDefault()
-        const href = this.getAttribute('href').substring(1)
-        const scrollTarget = document.getElementById(href)
-        const topOffset = 0
-        const elementPosition = scrollTarget.getBoundingClientRect().top
-        const offsetPosition = elementPosition - topOffset
-        
+        e.preventDefault();
+        const href = this.getAttribute('href').substring(1);
+        const scrollTarget = document.getElementById(href);
+        const topOffset = 0;
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
         window.scrollBy({
             top: offsetPosition,
             behavior: 'smooth',
-        })
-    })
-})
+        });
+    });
+});
+
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter) => {
+    let count = 0;
+    const target = parseInt(counter.getAttribute('data-target'));
+    const delay = parseInt(counter.getAttribute('delay'));
+
+    const updateCount = () => {
+
+    if (count === target) {
+        count = 0;
+    } else {
+        ++count;
+    }
+
+    if (count < 10){
+        counter.innerHTML = count;
+    } else {
+        counter.innerHTML = count + "+";
+    }
+    
+    setTimeout(updateCount, delay);
+  };
+  updateCount();
+});
